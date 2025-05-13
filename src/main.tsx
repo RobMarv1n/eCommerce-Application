c0709c309585180c828faed77355ed37f2f6832d
Aleksey <howlight19@gmail.com>
2025-05-13 14:17:44 +0300

    feat: add base styles, fonts and favicon

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app/App.tsx';
+import './app/styles/main.css';
 
 createRoot(document.querySelector('#root')!).render(
   <StrictMode>
