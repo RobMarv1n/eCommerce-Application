d372ac8e044440cc6eb2be225d814bb00113a062
Rob Marvin <fenalfan@gmail.com>
2025-05-18 21:11:23 +0300

    feat: add RegistrationFieldState type and update RegistrationFormData type

--- src/pages/registration/model/types.ts
+++ src/pages/registration/model/types.ts
@@ -1,16 +1,27 @@
+import { Country } from 'postal-code-validator';
+import { FieldError } from 'react-hook-form';
+
 export type RegistrationFormData = {
   email: string;
   password: string;
   firstName: string;
   lastName: string;
   birthDate: string;
-  address: RegistrationAddress;
-  country: string;
+  shippingAddress: RegistrationAddress;
+  billingAddress: RegistrationAddress;
 };
 
 export type RegistrationAddress = {
   street: string;
   city: string;
   zipCode: string;
-  country: string;
+  country: Country;
+};
+
+export type RegistrationFieldState = {
+  invalid: boolean;
+  isDirty: boolean;
+  isTouched: boolean;
+  isValidating: boolean;
+  error?: FieldError;
 };
