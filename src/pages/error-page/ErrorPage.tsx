b90b9648cfeea91a0699f48c16fd7889692c72ee
Aleksey <howlight19@gmail.com>
2025-05-19 00:32:12 +0300

    feat: add styles to Error page, also add LinkButton component

--- src/pages/error-page/ErrorPage.tsx
+++ src/pages/error-page/ErrorPage.tsx
@@ -1,13 +1,14 @@
-import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '../../types';
+import { LinkButton } from '../../shared/ui/LinkButton';
+import styles from './ErrorPage.module.css';
+import errorImage from './assets/img/404.png';
 
 export function ErrorPage() {
-  const navigate = useNavigate();
-
   return (
-    <>
-      <h1>Error 404</h1>
-      <button onClick={() => navigate(ROUTES.HOME)}>Back to Home</button>
-    </>
+    <section className={styles.errorPage}>
+      <img src={errorImage} alt="404 page not found" width="584" height="355" />
+      <h1 className={styles.errorPageTitle}>Oops! page not found</h1>
+      <LinkButton to={ROUTES.HOME}>Back to Home</LinkButton>
+    </section>
   );
 }
