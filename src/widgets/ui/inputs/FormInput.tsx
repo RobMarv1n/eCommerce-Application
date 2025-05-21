672ece76ecafea54ccad27b34794c6cbee270542
Rob Marvin <fenalfan@gmail.com>
2025-05-22 01:47:36 +0300

    feat: add a reusable FormInput component

---
+++ src/widgets/ui/inputs/FormInput.tsx
@@ -0,0 +1,38 @@
+import { FieldValues } from 'react-hook-form';
+import { InputProperties } from '../../../types';
+
+export function FormInput<T extends FieldValues>(
+  properties: InputProperties<T>
+) {
+  const {
+    className = 'form-input',
+    type = 'text',
+    name,
+    label,
+    id,
+    placeholder,
+    rules,
+    register,
+    errors,
+  } = properties;
+  const errorMessage = errors?.[name]?.message;
+
+  return (
+    <>
+      <label className="form-label" htmlFor={id}>
+        {label}
+      </label>
+      <input
+        {...register(name, rules)}
+        className={className}
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        id={id}
+      />
+      {errorMessage && (
+        <div className="validation-error">{errorMessage.toString()}</div>
+      )}
+    </>
+  );
+}
