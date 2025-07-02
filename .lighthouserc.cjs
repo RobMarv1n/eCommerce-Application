9f12cf3b650515a06c1dd11635e59d908450db17
RobMarv1n <88106847+RobMarv1n@users.noreply.github.com>
2025-07-02 06:54:36 +0300

    ci: add ci lighthouse
    * ci: add ci lighthouse
    
    * chore(config): add lighthouse configuration
    
    * chore(config): update lighthouse config
    
    * chore(config): add script for lighthouse in package.json
    
    * chore(config): update lighthouse ci
    
    * chore(config): update lighthouse config
    
    * chore(config): update config for deploy in gh-pages
    
    * chore(config): fix the performance check on warn
    
    * chore(config): change publish dir for deploy
    
    * chore(config): update configuration for deploy
    
    * chore(config): fix config structure
    
    * chore(config): add delete files on gh-pages before merge
    
    * chore(config): add new file pattern and change deploy branch
    
    * chore(config): update config file
    
    * chore(config): add lighthouse install, update configs
    
    * chore(config): delete deploy run, add temporaryPublicStorage
    
    * chore(config): fix target in config
    
    * chore(config): add links to reports in PR
    
    * chore(config): add page display in reports
    
    * chore(config): add the output of metrics in the comments
    
    * chore(config): fix config errors
    
    * chore(config): fix bug with incorrect json file
    
    * chore(config): fix bug with incorrect echo
    
    * chore(config): fix path for lighthouseci
    
    * chore(config): update config files
    
    * chore(config): add gh-token to ci.yml
    
    * chore(config): add permissions in ci.yml
    
    * chore(config): update ci.yml
    
    * chore(config): updater ci.yml
    
    * chore(config): change target to filesystem
    
    * chore(config): update configs

---
+++ .lighthouserc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: [
+        'https://ecobazar-sprint4.netlify.app/',
+        'https://ecobazar-sprint4.netlify.app/catalog',
+        'https://ecobazar-sprint4.netlify.app/product',
+        'https://ecobazar-sprint4.netlify.app/login',
+        'https://ecobazar-sprint4.netlify.app/registration',
+        'https://ecobazar-sprint4.netlify.app/profile',
+        'https://ecobazar-sprint4.netlify.app/cart',
+        'https://ecobazar-sprint4.netlify.app/about',
+      ],
+      exitOnFailure: false,
+      numberOfRuns: 1,
+      settings: {
+        chromeFlags:
+          '--no-sandbox --headless --disable-gpu --disable-dev-shm-usage',
+        disableStorageReset: true,
+        chromePath:
+          'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+      },
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['warn', { minScore: 0.7 }],
+        'categories:accessibility': ['warn', { minScore: 0.7 }],
+        'categories:best-practices': ['warn', { minScore: 0.7 }],
+        'categories:seo': ['warn', { minScore: 0.7 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
