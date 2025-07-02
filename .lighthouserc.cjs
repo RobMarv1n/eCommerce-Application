95f31398658d2b17fa36b97f0385213b73c33fcf
Rob Marvin <fenalfan@gmail.com>
2025-07-02 03:20:04 +0300

    chore(config): fix the performance check on warn

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -23,7 +23,7 @@ module.exports = {
     },
     assert: {
       assertions: {
-        'categories:performance': ['error', { minScore: 0.7 }],
+        'categories:performance': ['warn', { minScore: 0.7 }],
         'categories:accessibility': ['warn', { minScore: 0.7 }],
         'categories:best-practices': ['warn', { minScore: 0.7 }],
         'categories:seo': ['warn', { minScore: 0.7 }],
